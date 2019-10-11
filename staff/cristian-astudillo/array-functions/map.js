6d6bbf62abf53109995fce417e6b0d74ca463bf5
Cristian Astudillo <astudillo82@gmail.com>
2019-10-11 16:16:29 +0200

    update map file

--- staff/cristian-astudillo/array-functions/map.js
+++ staff/cristian-astudillo/array-functions/map.js
@@ -4,7 +4,7 @@
  * @param {Function} expression The expression to evaluate in each item of the array.
  */
 
-debugger
+
 function map(array,expression) {
     var newArray = []
     for (let i = 0; i < array.length; i++) {
