695def06b3ba0d952318d0a1ad99eccd2c56d437
Cristian <astudillo82@gmail.com>
2019-12-17 11:39:51 +0100

    ¨upgrade

--- staff/cristian-astudillo/rock-blog/rock-api/routes/posts/modify/index.js
+++ staff/cristian-astudillo/rock-blog/rock-api/routes/posts/modify/index.js
@@ -4,17 +4,10 @@ module.exports = async(req,res) => {
     try {
         const {id, body: { owner, title, description } } = req
         await logic.modifyPost(id, {owner, title, description})
-        
+
         res.status(201).json({ message :'Post was modify correctly'})
         
     } catch ({ message }) {
         res.status(400).json({ error: message })
     }
 }
-
-
-
-
-
-
-
